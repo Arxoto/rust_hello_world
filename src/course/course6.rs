ce11bfc2cb948ed2f25a1588c1a9e8302ec1e02d
Arxoto <1484663586@qq.com>
2023-09-10 19:52:56 +0800

    增加 panic 部分理解

--- src/course/course6.rs
+++ src/course/course6.rs
@@ -10,8 +10,25 @@ mod error_handle {
     /// - rust 【不建议捕获】 见 std::panic::catch_unwind() 有一些限制：传入的闭包要实现 UnwindSafe
     #[test]
     fn panic_error() {
-        panic!("error occured");
+        // panic!("error occured");
         // println!("Hello, Rust"); // 中断 程序终止
+    }
+
+    /// 当出现 panic 时，程序默认会开始 展开（unwinding），这意味着 Rust 会回溯栈并清理它遇到的每一个函数的数据，不过这个回溯并清理的过程有很多工作。
+    ///
+    /// 另一种选择是直接 终止（abort），这会不清理数据就退出程序。那么程序所使用的内存需要由操作系统来清理。
+    ///
+    /// 如果你需要项目的最终二进制文件越小越好，panic 时通过在 Cargo.toml 的 [profile(.release)] 部分增加 panic = 'abort'，可以由展开切换为终止。
+    #[test]
+    fn panic_catch() {
+        assert!(std::panic::catch_unwind(|| { 0 }).is_ok());
+
+        // // 注意 std::panic::catch_unwind 只能捕获 unwind stack 形式的 panic
+        // let result = std::panic::catch_unwind(|| { 0 });
+        // match result {
+        //     Ok(n) => println!("{}", n),
+        //     Err(_) => println!("panic!"),
+        // }
     }
 
     #[test]
