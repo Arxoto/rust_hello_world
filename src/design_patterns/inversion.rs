0ad80239bdd2ff9b3d150149e4c65d705d12c921
Arxoto <1484663586@qq.com>
2024-11-25 00:14:01 +0800

    去除魔法参数中的不必要的生命周期标注

--- src/design_patterns/inversion.rs
+++ src/design_patterns/inversion.rs
@@ -1,6 +1,6 @@
 //! 反转  
 //! 实现类似于控制反转的效果  
-//! 但是好像没啥用 反正还是需要直接读取上下文类型里的内容  
+//! 一般用于框架 如 Bevy 或者 Axum  
 //!
 //! 这里增加对 静态分发 和 动态分发 的说明
 //! - 静态分发（泛型） 编译期单态化 存在类型膨胀（编译时间和二进制文件）及缓存失效概率增加等问题
@@ -24,32 +24,30 @@ pub struct Context {
 /// 其次定义一个抽象特征 支持从上下文中进行类型转换  
 /// 这里有无损转换的需求 因此用到生命周期注释  
 /// 这个特征可以由 使用方 根据需求自由实现
-pub trait FromContext<'a> {
-    fn from_context(ctx: &'a Context) -> Self;
+pub trait FromContext {
+    fn from_context(ctx: &Context) -> Self;
 }
 
 /// 再者定义一个抽象的代理函数  
-/// 这里的的生命周期和泛型声明其实一开始不需要 是在做多态实现的时候带进来的约束
-///
-/// 接着精彩的来了 为多个参数的 Func 实现该特征 完成函数与函数之间的转换  
-/// 这一步需要由框架测来实现
-trait Handler<'a, T> {
-    fn call(self, ctx: &'a Context);
+/// 泛型 T 允许对于不同类型分别不同实现 起到类似于重载的作用
+/// 需要框架侧多多个 Func 做实现
+trait Handler<T> {
+    fn call(self, ctx: &Context);
 }
 
-impl<'a, T: FromContext<'a>, F: Fn(T)> Handler<'a, T> for F {
-    fn call(self, ctx: &'a Context) {
+impl<T: FromContext, F: Fn(T)> Handler<T> for F {
+    fn call(self, ctx: &Context) {
         self(T::from_context(ctx));
     }
 }
 
-impl<'a, T1, T2, F> Handler<'a, (T1, T2)> for F
+impl<T1, T2, F> Handler<(T1, T2)> for F
 where
     F: Fn(T1, T2),
-    T1: FromContext<'a>,
-    T2: FromContext<'a>,
+    T1: FromContext,
+    T2: FromContext,
 {
-    fn call(self, ctx: &'a Context) {
+    fn call(self, ctx: &Context) {
         self(T1::from_context(ctx), T2::from_context(ctx));
     }
 }
@@ -58,7 +56,7 @@ where
 /// - 注册函数 由使用方将定制化的逻辑注入（这里没举例）
 /// - 触发函数 由框架侧根据生命周期自动调用
 #[inline]
-fn trigger<'a, T>(ctx: &'a Context, handler: impl Handler<'a, T>) {
+fn trigger<T>(ctx: &Context, handler: impl Handler<T>) {
     handler.call(ctx);
 }
 
@@ -68,7 +66,7 @@ mod test {
 
     pub struct Id(pub u32);
 
-    impl FromContext<'_> for Id {
+    impl FromContext for Id {
         fn from_context(ctx: &Context) -> Self {
             Id(ctx.id)
         }
@@ -80,11 +78,11 @@ mod test {
     }
 
     /// 对于复杂的对象 使用引用 标明生命周期
-    pub struct Param<'a>(pub &'a String);
+    pub struct Param(pub String);
 
-    impl<'a> FromContext<'a> for Param<'a> {
-        fn from_context(ctx: &'a Context) -> Self {
-            Param(&ctx.param)
+    impl FromContext for Param {
+        fn from_context(ctx: &Context) -> Self {
+            Param(ctx.param.clone())
         }
     }
 
